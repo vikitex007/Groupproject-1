bc1dfd097e577b4f3679a501989419d77c44a91a
Ukran004 <110590995+Ukran004@users.noreply.github.com>
2023-01-08 14:18:36 +0545

    changes

--- login.py
+++ login.py
@@ -18,12 +18,10 @@ head = Label(win,text='Login page')
 head.pack(side='top')
 body = Label(win,text='Welcome to siddhartha Bus travel',font='Rockwell 12').place(x=150,y=40)
 #adding text in login page
-Name = Label(win,text='Name',bg='black',fg='white').place(x=100,y=100)
+Pkone_Number = Label(win,text='Phone Number',bg='black',fg='white').place(x=100,y=100)
 e1=Entry(win).place(x=200,y=100)
-Address= Label(win,text='Address',bg='black',fg='white').place(x=100,y=150)
+Password= Label(win,text='Password',bg='black',fg='white').place(x=100,y=150)
 e2=Entry(win).place(x=200,y=150)
-Phone = Label(win,text='Number',bg='black',fg='white').place(x=100,y=200)
-e3=Entry(win).place(x=200,y=200)
 
 def click():
     a = messagebox.showinfo('congratulations','You are sucessfully logged in')
