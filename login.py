9200bbfd04330ddb257ea52a1ec1958a55fefaa6
sachin shrestha <110590972+vikitex007@users.noreply.github.com>
2023-01-08 21:43:53 +0545

    changes

--- login.py
+++ login.py
@@ -1,31 +1,50 @@
-
+#===========importing required module==========
 from tkinter import *
 from PIL import Image,ImageTk
 from tkinter import messagebox
+
 win = Tk()
+#=========Title===========
 win.title('Bus management system')
-#keeping title name
+
+#======keeping icon=======
 win.iconbitmap('icon.ico')
-#inserting icon
-win.geometry('700x350')
 
-image = Image.open('bus.png')
+#======giving geometry of page==============
+win.geometry('1199x600+100+50')
+win.resizable(False,False)
 
+
+#======adding backgroud image=========
+image = Image.open('buss.png')
 my_image = ImageTk.PhotoImage(image)
-label = Label(win,image = my_image).pack()
+label = Label(win,image = my_image).place(x=0,y=0,relheight=1,relwidth=1) 
 
-head = Label(win,text='Login page')
-head.pack(side='top')
-body = Label(win,text='Welcome to siddhartha Bus travel',font='Rockwell 12').place(x=150,y=40)
-#adding text in login page
-Phone_Number = Label(win,text='Phone Number',bg='black',fg='white').place(x=100,y=100)
-e1=Entry(win).place(x=200,y=100)
-Password= Label(win,text='Password',bg='black',fg='white').place(x=100,y=150)
-e2=Entry(win).place(x=200,y=150)
 
-def click():
-    a = messagebox.showinfo('congratulations','You are sucessfully logged in')
+#====adding Frame Login===
+frame_login = Frame(win,bg='#90a8bd')
+frame_login.place(x=450,y=80,height=360,width=500)
 
-btn=Button(win,text='Login',command=click).place(x=150,y=230)
+#=====for title,label,entry inside frame=======
+title=Label(frame_login,text='Login Here',font=('ariel',22,'bold'),bg='#90a8bd' ).place(x=180,y=30)
+descript=Label(frame_login,text='Passengers Login Area',font=('Goudy old style',17,'bold'),bg='#90a8bd').place(x=160,y=70)
+Phone_number=Label(frame_login,text='Number',font=('Goudy old style',14,'bold'),bg='#90a8bd').place(x=50,y=120)
+textbox1= Entry(frame_login,font=('times new roman',15)).place(x=50,y=150)
+password=Label(frame_login,text='Password',font=('Goudy old style',14,'bold'),bg='#90a8bd').place(x=50,y=180)
+textbox2= Entry(frame_login,font=('times new roman',15)).place(x=50,y=210)
+forget_button= Button (frame_login,text='Forget password?',bg='#90a8bd',bd=0,font=('times new roman',12)).place(x=50,y=240)
+Login_button= Button (win,text='Login',bg='#90a8bd',font=('times new roman',11)).place(x=650,y=420,width=120,height=40)
+
+
+#=========defining function============
+def login_func():
+    if password.get()=='' or Phone_number()=='':
+        messagebox.showerror('Error','All fields are requied',parent=win)
+    elif password.get()!='123456' or Phone_number()!='9810119909':
+        messagebox.showerror('Error','Invalid phone number or password',parent=win)
+    else:
+        messagebox.showinfo('Welcome','succesfully logged in')
 
 win.mainloop()
+
+
