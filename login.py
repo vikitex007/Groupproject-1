b1b70e0d922cf1a5365f06dd811512f3192b87c4
sachin shrestha <110590972+vikitex007@users.noreply.github.com>
2023-01-26 21:19:42 +0545

    changes in background

--- login.py
+++ login.py
@@ -16,40 +16,42 @@ win.resizable(False,False)
 
 
 #======adding backgroud image=========#
-image = Image.open('buss.png')
+image = Image.open('bus.png')
 my_image = ImageTk.PhotoImage(image)
 label = Label(win,image = my_image).place(x=0,y=0,relheight=1,relwidth=1) 
 
 
 #====adding Frame Login===#
-frame_login = Frame(win,bg='#90a8bd')
-frame_login.place(x=450,y=80,height=360,width=500)
+frame_login = Frame(win,bg='white')
+frame_login.place(x=600,y=60,height=450,width=400)
 
 #=====for title,label,entry inside frame=======#
-title=Label(frame_login,text='Login Here',font=('ariel',22,'bold'),bg='#90a8bd' ).place(x=180,y=30)
-descript=Label(frame_login,text='Passengers Login Area',font=('Goudy old style',17,'bold'),bg='#90a8bd').place(x=160,y=70)
+# title=Label(frame_login,text='Hello',font=('Alegreya',22,'bold'),bg='white' ).place(x=125,y=30)
+descript=Label(frame_login,text='Welcome to our login page',font=('Alegreya',17),bg='white').place(x=60,y=40)
 
-Phone_number=Label(frame_login,text='Number',font=('Goudy old style',14,'bold'),bg='#90a8bd').place(x=50,y=120)
-textbox1= Entry(frame_login,font=('times new roman',15)).place(x=50,y=150)
+Phone_number=Label(frame_login,text='Number',font=('Goudy old style',14,'bold'),bg='white').place(x=140,y=120)
+textbox1= Entry(frame_login,font=('times new roman',15)).place(x=90,y=150)
 
-password=Label(frame_login,text='Password',font=('Goudy old style',14,'bold'),bg='#90a8bd').place(x=50,y=180)
-textbox2= Entry(frame_login,font=('times new roman',15),show='*').place(x=50,y=210)
+password=Label(frame_login,text='Password',font=('Goudy old style',14,'bold'),bg='white').place(x=140,y=180)
+textbox2= Entry(frame_login,font=('times new roman',15),show='*').place(x=90,y=210)
 
+check = Checkbutton(frame_login,text='Remember details?',bg='white').place(x=90,y=240)
+ 
+or_b = Label(frame_login,text="Don't have account?",bg = 'white',font =('Goudy old style',12,'bold') ).place(x=90,y=320)
 
-
-
-
+signup_button= Button (frame_login,text='Sign Up',bg='white',font=('italic',11)).place(x=230,y=320,width=120,height=0)
 
 #=========defining function============#
 def login_func():
     if textbox1=='' or textbox2()=='':
-        messagebox.showerror('Error','All fields are requied',parent=Tk)
+        messagebox.showerror('Error','All fields are requied',parent=win)
     elif textbox2()!='123456' or textbox1!='9810119909':
-        messagebox.showerror('Error','Invalid phone number or password',parent=Tk)
+        messagebox.showerror('Error','Invalid phone number or password',parent=win)
     else:
         messagebox.showinfo('Welcome','succesfully logged in')
+    return()
         
-Login_button= Button (win,text='Login',bg='#90a8bd',command=login_func,font=('times new roman',11)).place(x=650,y=420,width=120,height=40)
+Login_button= Button (frame_login,text='Login',bg='white',command=login_func,font=('italic',11)).place(x=90,y=270,width=120,height=40)
 
 win.mainloop()
 
